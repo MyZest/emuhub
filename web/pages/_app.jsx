79ec66da459e2210e9550282e980f8c044555109
zest <mrwangl2018@163.com>
2025-12-09 04:57:29 +0800

    feat: 前端加入meta标签

---
+++ web/pages/_app.jsx
@@ -0,0 +1,22 @@
+import '../styles/globals.css'
+import Head from 'next/head'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="referrer" content="never" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
+        <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+        <meta httpEquiv="Pragma" content="no-cache" />
+        <meta httpEquiv="Expires" content="0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
